940d39674a58e74b111d89e1e1f9d955d828da16
Jonathan Kria <jona1115@iastate.edu>
2024-05-27 18:23:55 -0700

    Hmm

--- python_tflite/custom_conv.c
+++ python_tflite/custom_conv.c
@@ -1,21 +1,16 @@
-// custom_conv.c
-#include "tensorflow/lite/c/common.h"
-// #include "/home/petalinux/tensorflow/tensorflow/lite/c/common.h"
-// #include "/home/petalinux/tensorflow/tensorflow/lite/c/c_api_internal.h"
-
-#include <cstring>
-#include <cstdlib>
+// Compile using: g++ -shared -o libcustom_conv_delegate.so -fPIC custom_conv.c -I/usr/local/include -L/usr/local/lib -ltensorflowlite_c
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tensorflow/lite/c/common.h"
 
 // Example custom convolution implementation
 TfLiteStatus CustomConv2D(TfLiteContext* context, TfLiteNode* node) {
     printf("Custom convolution called\n");
 
     // Get input and output tensors
-    TfLiteTensor* input = &context->tensors[node->inputs->data[0]];
+    const TfLiteTensor* input = &context->tensors[node->inputs->data[0]];
     TfLiteTensor* output = &context->tensors[node->outputs->data[0]];
 
     // Perform custom convolution (dummy implementation for demonstration)
@@ -24,18 +19,35 @@ TfLiteStatus CustomConv2D(TfLiteContext* context, TfLiteNode* node) {
     return kTfLiteOk;
 }
 
-// Replace the convolution operator with custom implementation
-TfLiteStatus ReplaceConvolutionOperator(TfLiteContext* context) {
-    for (int i = 0; i < context->tensors_size; ++i) {
-        const TfLiteRegistration* registration = context->GetNodeAndRegistration(context, i, NULL);
-        if (registration != NULL && registration->builtin_code == kTfLiteBuiltinConv2d) {
-            // Get node and modify the invoke function
-            TfLiteNode* node;
-            context->GetNodeAndRegistration(context, i, &node);
-            node->builtin_code = registration->builtin_code;
-            node->user_data = registration->user_data;
-            node->delegate = registration->delegate;
-            ((TfLiteRegistration*)registration)->invoke = CustomConv2D;
+// Delegate data structure
+typedef struct {
+    TfLiteDelegate base;
+    const TfLiteRegistration* original_registration;
+} CustomDelegate;
+
+// Delegate node replacement function
+TfLiteStatus CustomDelegateReplaceNode(TfLiteContext* context, TfLiteNode* node, TfLiteRegistration* registration, CustomDelegate* custom_delegate) {
+    // Save the original registration and replace the invoke function
+    custom_delegate->original_registration = registration;
+    registration->invoke = CustomConv2D;
+    return kTfLiteOk;
+}
+
+// Prepare function for the delegate
+TfLiteStatus CustomDelegatePrepare(TfLiteContext* context, TfLiteDelegate* delegate) {
+    CustomDelegate* custom_delegate = (CustomDelegate*)delegate;
+    TfLiteIntArray* execution_plan;
+    TF_LITE_ENSURE_STATUS(context->GetExecutionPlan(context, &execution_plan));
+
+    for (int i = 0; i < execution_plan->size; ++i) {
+        TfLiteNode* node;
+        TfLiteRegistration* registration;
+        TF_LITE_ENSURE_STATUS(context->GetNodeAndRegistration(context, execution_plan->data[i], &node, &registration));
+
+        // Check if the node is a convolution operation
+        const int kTfLiteBuiltinConv2d = 3;  // Using the numeric value for Conv2D operation
+        if (registration->builtin_code == kTfLiteBuiltinConv2d) {
+            CustomDelegateReplaceNode(context, node, registration, custom_delegate);
         }
     }
     return kTfLiteOk;
@@ -43,16 +55,17 @@ TfLiteStatus ReplaceConvolutionOperator(TfLiteContext* context) {
 
 // Create a custom delegate that overrides the convolution operator
 TfLiteDelegate* CreateCustomConvDelegate() {
-    TfLiteDelegate* delegate = (TfLiteDelegate*)malloc(sizeof(TfLiteDelegate));
-    if (!delegate) return NULL;
+    CustomDelegate* custom_delegate = (CustomDelegate*)malloc(sizeof(CustomDelegate));
+    if (!custom_delegate) return NULL;
 
-    delegate->Prepare = [](TfLiteContext* context, TfLiteDelegate* delegate) -> TfLiteStatus {
-        return ReplaceConvolutionOperator(context);
-    };
+    custom_delegate->base.Prepare = CustomDelegatePrepare;
+    custom_delegate->base.flags = kTfLiteDelegateFlagsNone;
+    custom_delegate->original_registration = NULL;
 
-    delegate->CopyFromBufferHandle = NULL;
-    delegate->CopyToBufferHandle = NULL;
-    delegate->FreeBufferHandle = NULL;
+    return (TfLiteDelegate*)custom_delegate;
+}
 
-    return delegate;
+// Delegate destroy function (if needed)
+void DestroyCustomConvDelegate(TfLiteDelegate* delegate) {
+    free(delegate);
 }
